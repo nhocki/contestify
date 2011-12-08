48db55714c414884f5f94ee400ae2608d93a74a5
Nicolás Hock Isaza <nhocki@gmail.com>
2011-12-08 16:45:42 -0500

    Adding new interface for `Contestify::Uploader`.
    Now the `Contestify::Uploader.upload!` method will receive an array with the
    problems path. This way, we can extract the uploading functionality to any
    future strategy for different problems sources.
    
    The paths **must** be absolute.

--- lib/contestify/uploader.rb
+++ lib/contestify/uploader.rb
@@ -1,11 +1,24 @@
 module Contestify
+  # <tt>Contestify::Uploader</tt> is a simple class that will habdle the
+  # uploading part to the server.
   class Uploader
-    def self.upload!(server_url, admin_pwd, base_dir)
+    # <tt>Contestify::Uploader</tt> is *the only* method you should call
+    # to upload the problems to the server. It will receive 3 params.
+    #
+    # ==== Parameters
+    # server_url<String>:: The **upload** URL in the server. E.g http://your-dom-judge.com/jury/problem.php
+    #
+    # admin_pwd<String>:: This is the `admin` password needed to authenticate in the server
+    #
+    # problems_path<Array>:: An array having all the paths where the problems input/output are.
+    #                        This *should* be the return array from a <tt>Contestify::Configuration</tt> call.
+    #                        The paths here **must** be absolute paths.
+    #
+    def self.upload!(server_url, admin_pwd, problems_path)
       puts green "\n\n=> Uploading problems to the server"
-
-      Dir.chdir base_dir # Ensure you're on the `root` dir
-      Dir.glob("*").select { |f| File.directory?(f) }.each do |dir|
-        Dir.chdir File.join(base_dir, dir)
+      
+      for dir in problems_path do
+        Dir.chdir dir
 
         unless File.exists?("domjudge-problem.ini")
           puts cyan "domjudge-problem.ini not found. Skipping #{dir}."
@@ -18,9 +31,10 @@ module Contestify
         raise Exception.new(red Contestify::ZIP_PROBLEM) unless $?.success?
 
         puts blue "===> Uploading #{dir}/bundle.zip..."
-        curl_output = system("curl -vv -F upload=Upload -F problem_archive=@bundle.zip --user admin:#{admin_pwd} #{server_url}")
+        # curl_output = system("curl -vv -F upload=Upload -F problem_archive=@bundle.zip --user admin:#{admin_pwd} #{server_url}")
         puts blue "===> Upload done\n"
       end
+
       puts green "\n\n=> All problems have been uploaded"
     end
   end
